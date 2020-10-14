82ef7943e2641ce9bb4ab2e04ead1cfb70dabf08
hisa <hisayuki.k8@gmail.com>
2020-10-14 22:32:45 +0900

    [updata]

---
+++ src/practice/chapter1.js
@@ -0,0 +1,22 @@
+// let a: object = {
+//     b: 'x'
+// }
+// a.b
+// let a = {
+//     b: 'x'
+// }
+// console.log(a.b)
+// let b = {
+//     c: {
+//         d: 'f'
+//     }
+// }
+// console.log(b.c)
+// let a: {b: number} = {
+//     b: 12
+// }
+// console.log(a.b)
+var a = {
+    b: 12
+};
+console.log(a.b);
---
+++ src/practice/chapter1.ts
@@ -0,0 +1,37 @@
+// let a: object = {
+//     b: 'x'
+// }
+
+// a.b
+
+
+// let a = {
+//     b: 'x'
+// }
+// console.log(a.b)
+
+// let b = {
+//     c: {
+//         d: 'f'
+//     }
+// }
+// console.log(b.c)
+
+
+
+
+
+// let a: {b: number} = {
+//     b: 12
+// }
+
+// console.log(a.b)
+
+
+
+
+// const a: {b: number} = {
+//     b: 12
+// }
+
+// console.log(a.b)
