a5d2cb3c2250a0ea3855420c036d0edc97fa41d9
hisa <hisayuki.k8@gmail.com>
2020-10-16 21:43:49 +0900

    [start]chapter_function

---
+++ src/chapter3/exercises.js
@@ -0,0 +1,16 @@
+var a = 1042; //number
+var b = 'apple and oranges'; //string
+var c = 'pineapples'; //string
+var d = [true, true, true]; //enum
+var e = { type: 'ficus' }; //string
+var f = [1, false]; //[number, enum]
+var g = [3]; //number[]
+var h = null; //null
+console.log(typeof (a)); // number
+console.log(typeof (b)); // string
+console.log(typeof (c)); // 'pineapples'
+console.log(typeof (d)); // boolean[]
+console.log(typeof (e)); // {type: string}
+console.log(typeof (f)); // (number | boolean)
+console.log(typeof (g)); // number[]
+console.log(typeof (h)); // any
