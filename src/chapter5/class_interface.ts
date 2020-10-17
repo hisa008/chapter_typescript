0b4956baa260bf36f78298ea4e7f3b5aed5d88fd
hisa <hisayuki.k8@gmail.com>
2020-10-18 02:09:50 +0900

    [start]chapter5

---
+++ src/chapter5/class_interface.ts
@@ -0,0 +1,37 @@
+// class Game {} // チェスのゲームを表す
+// class Piece {} // チェスの駒
+// class Position {} // 駒の座標
+
+// class King extends Piece {} // キング
+// class Queen extends Piece {} // クイーン
+// class Bishop extends Piece {} // ビショップ
+// class Knight extends Piece {} // ナイト
+// class Rook extends Piece {} // ルーク
+// class Pawn extends Piece {} // ポーン
+
+
+// type Color = 'Black' | 'White'
+// type File1 = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
+// type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 
+
+// class Position1 {
+//     constructor(
+//         private file: File1,
+//         private rank: Rank
+//     ) {}
+// }
+
+// class Piece1 {
+//     protected position: Position
+//     constructor(
+//         private readonly color: Color,
+//         file: File1,
+//         rank: Rank
+//     ) {
+//         this.position = new Position(file, rank)
+//     }
+// }
+
+
+
+let 
